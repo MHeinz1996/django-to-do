27ee33481206e7a8bbb85f1812d7d66d395bf790
MHeinz1996 <michael.heinzinger@gmail.com>
2022-07-12 14:26:58 -0500

    Made migrations and migrated

---
+++ todo_proj/todo_app/models.py
@@ -0,0 +1,19 @@
+from tkinter import W
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class AppUser(AbstractUser):
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    owner = models.ForeignKey(AppUser, on_delete=models.CASCADE)
